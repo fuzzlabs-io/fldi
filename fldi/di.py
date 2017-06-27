eb01cfe305386dd29ba754293080f02aec3f2a31
Ádám Gólya <adam.stork@gmail.com>
2017-06-27 10:42:29 +0200

    feat(fldi): add basic di container
    In the follow up patches we have to extend
    it with handling implementations.

---
+++ fldi/di.py
@@ -0,0 +1,45 @@
+import inspect
+import dotmap
+
+class DIError(Exception):
+    pass
+
+class Container (object):
+
+    __dependencies = {}
+    __container = {}
+
+    def register(self, dependency, name):
+        self.__dependencies[name] = dependency
+        return self
+
+    def get(self, name):
+        if (name in self.__container):
+            return self.__container[name]
+
+        dependency = self.__getDependencyByName(name)
+        classDependency = self.__getClassDependency(dependency)
+
+        self.__container[name] = classDependency
+        return self.__container[name]
+
+    def __getDependencyByName(self, name):
+        try:
+            return self.__dependencies[name]
+        except:
+            raise DIError('Can not find the service ' + name)
+
+    def __getClassDependency(self, dependency):
+        args = []
+        if (inspect.isfunction(dependency)):
+            for arg in inspect.getargspec(dependency)[0]:
+                args.append(self.get(arg))
+            return dotmap.DotMap(dependency(*args))
+
+        if (inspect.ismethod(dependency.__init__)):
+            for arg in inspect.getargspec(dependency.__init__)[0]:
+                if (arg != "self"):
+                    args.append(self.get(arg))
+        return dependency(*args)
+
+
