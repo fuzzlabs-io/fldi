a09a9512d7e1b7775436b31eaa644596441295e1
Ádám Gólya <adam.stork@gmail.com>
2017-06-27 10:40:08 +0200

    feat(fldi): initial add of fldi

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+try:
+    long_description = open("README.rst").read()
+except IOError:
+    long_description = ""
+
+setup(
+    name="fldi",
+    version="0.1.0",
+    description="A dependency injection container",
+    license="MIT",
+    author="Ádám Gólya",
+    packages=find_packages(),
+    install_requires=['dotmap'],
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+    ]
+)
